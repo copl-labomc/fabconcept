820e9f793d0dbde79844cd5f88d277b624ad2042
JOGAU227 <joseph.gaulin.1@ulaval.ca>
2022-01-11 10:50:32 -0500

    Update Arduino_control.py
    Modification des input output

--- Fiber_Drawing_Tower/Arduino_control.py
+++ Fiber_Drawing_Tower/Arduino_control.py
@@ -1,7 +1,7 @@
 # This code is use to controle the ARDUINO wich is commanding all the systemes of the
 # tower. For exemple the motors, the KEYENCE tool and the temperature controler.
 import time # Using module time
-import keyboard #Using module keyboard 
+# import keyboard #Using module keyboard 
 
 # Instale pyfirmata library if it isn't already
 try:
@@ -18,7 +18,8 @@ it = util.Iterator(board)
 it.start()
 
 #Read the 3 output
-pin3 = board.get_pin('o:3:p')
+analog_0 = board.get_pin('a:0:i')
 
 while True:
-    print(pin3.read())
+    time.sleep(0.2)
+    print(analog_0.read())
