1f63d8a2304328d31acbf17b832a8e9e4a56eaba
alexmomo1212 <almor259@ulaval.ca>
2024-05-13 15:16:41 -0400

    Code rearanging

--- Fiber_Drawing_Tower/gui_V3.py
+++ Fiber_Drawing_Tower/gui_V3.py
@@ -5,6 +5,7 @@ import time
 import serial
 
 timer = time.time()
+
 ## CONNECTION SECTION 
 # create a COM4 communication "ideally" this should be a droplist menu 
 
@@ -26,18 +27,47 @@ def serial_ports():
             pass
     return result
 
-Available_ports = serial_ports()
 
-commPort = Available_ports[0]
 
-global ser
-ser = serial.Serial(commPort, baudrate = 9600, timeout = 1)
+## Button section
 
 def close_window():
   """Close serial communication when the windows is closed"""
   global running
   running = False  # turn off while loop
   ser.close()
+
+
+# Preform Button functions
+def preform_up():
+    ser.write(b't')
+
+def preform_down():
+    ser.write(b'b')
+
+def stop_preform():
+    ser.write(b'k')
+
+# Cabestan Button functions
+def start_cabestan():
+    """send "s" in byte to  start cabestan motor
+    """
+    ser.write(b's')
+def stop_cabestan():
+    """send "a" in byte to stop cabestan motor
+    """
+    ser.write(b'a')
+
+def send_diameter():
+    """ send the desired diameter"""
+    entry = diameter_entry.get()
+    if entry != '':
+        ser.write(entry.encode())
+        #send end character
+        ser.write(b'e')
+
+
+
 
 ### GUI
 
@@ -54,18 +84,6 @@ root.protocol("WM_DELETE_WINDOW", close_window)
 # Frame
 preform_frame = tk.LabelFrame(root, text="Preform Motor", height=100,width=150)
 preform_frame.grid(row=0, column=0, rowspan=3, columnspan=3, padx=5, pady=5)
-
-
-# Button functions
-def preform_up():
-    ser.write(b't')
-
-def preform_down():
-    ser.write(b'b')
-
-def stop_preform():
-    ser.write(b'k')
-
 
 # Buttons of preform section
 
@@ -92,30 +110,12 @@ speed_preform.grid(row=3, column=0, padx=5)
 cabestan_frame = tk.LabelFrame(root, text="Cabestan Motor", height=100,width=150)
 cabestan_frame.grid(row=4, column=0, rowspan=3, columnspan=3)
 
-# Button functions
-def start_cabestan():
-    """send "s" in byte to  start cabestan motor
-    """
-    ser.write(b's')
-def stop_cabestan():
-    """send "a" in byte to stop cabestan motor
-    """
-    ser.write(b'a')
 
-def send_diameter():
-    """ send the desired diameter"""
-    entry = diameter_entry.get()
-    if entry != '':
-        ser.write(entry.encode())
-
-        ser.write(b'e')
-
-
-# Création du bouton Start en vert
+# Creation of green Start button
 button_start = tk.Button(cabestan_frame, text="Start", command=start_cabestan, font=("Segoe Ui", 12), bg="green", fg="white")
 button_start.grid(row=0, column=0, padx=5)
 
-# Création du bouton Stop en rouge
+# Creation of red Stop button
 button_stop = tk.Button(cabestan_frame, text="Stop", command=stop_cabestan, font=("Segoe Ui", 12), bg="red", fg="white")
 button_stop.grid(row=0, column=1, padx=5)
 
@@ -129,14 +129,14 @@ connection_frame = tk.LabelFrame(root, text="Connection (WIP)", height=50,width=
 connection_frame.grid(row=0, column=4, rowspan=2, columnspan=2, padx=5, pady=5)
 
 
-clicked = tk.StringVar() 
-  
-# initial menu text 
-clicked.set( "Default: COM4" ) 
+# Port selection menu
+clicked = tk.StringVar()
+Available_ports = serial_ports()
+clicked.set(Available_ports[0]) 
 connection_drop_menu = tk.OptionMenu(connection_frame, clicked, *Available_ports)
 connection_drop_menu.grid(row=0, column=0,columnspan=1, padx=5)
 
-connect_button = tk.Button(connection_frame, text = "Connect")
+connect_button = tk.Button(connection_frame, text = "Reconnect")
 connect_button.grid(row=0, column=1 , padx=5)
 
 status_label = tk.Label(connection_frame, text = "Status: Disconnected")
@@ -149,6 +149,7 @@ parameter_frame.grid(row=2, column=4, rowspan=3, columnspan=3, padx=5, pady=5)
 # printing diameter measured by laser sensor
 diameter = tk.Label(parameter_frame, text="Diameter Measurement:")
 diameter.grid(row=0, column=0,columnspan=4, padx=5)
+
 # input for the diameter desired
 diameter_desired = tk.Label(parameter_frame, text='Diameter desired :')
 diameter_desired.grid(row=1, column=0)
@@ -158,6 +159,7 @@ diameter_entry_button = tk.Button(parameter_frame, text = 'Send', command=send_d
 diameter_entry_button.grid(row=1, column = 2)
 
 
+## Debug section
 #Debug screen with time delay and received serial packets
 debug_frame = tk.LabelFrame(root, text="Debug", height=100,width=150)
 debug_frame.grid(row=5, column=4, rowspan=3, columnspan=3, padx=5, pady=5)
@@ -205,14 +207,30 @@ def checkSerialPort():
         pass
 running = True
 
+### Main program loop
 
-while True:
-    root.update()
-    if not running: 
-        break
-    checkSerialPort()
 
-# Closer serial communication 
+#Intialise Serial communication
+
+print(Available_ports)
+
+commPort = Available_ports[0]
+
+global ser
+ser = serial.Serial(commPort, baudrate = 9600, timeout = 1)
+
+def program_loop():
+    """Executes the main program loop when called"""
+    while True:
+        root.update()
+        if not running: 
+            break
+        checkSerialPort()
+
+#Run the main program loop
+program_loop()
+
+# Close serial communication 
 ser.close()
 
 
