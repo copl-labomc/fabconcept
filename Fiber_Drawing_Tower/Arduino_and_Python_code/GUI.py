61061c89d69a9f75f3a8ded87ea0dfa795ab6fee
alexmomo1212 <almor259@ulaval.ca>
2024-06-05 14:19:51 -0400

    class init

--- Fiber_Drawing_Tower/Arduino_and_Python_code/GUI.py
+++ Fiber_Drawing_Tower/Arduino_and_Python_code/GUI.py
@@ -1,8 +1,6 @@
 import tkinter as tk
 import serial
 from tkinter import ttk
-import serial
-
 
 def serial_ports():
     """ Finds all the port in use and returns it as a list
@@ -22,6 +20,138 @@ def serial_ports():
     if result == []:
         return ["None"]
     return result
+
+def port_write(command):
+    """Checks if the arduino is connected then sends the command trough the serial port"""
+    if status_label.cget("text") == "Connected":
+        ser.write(command)
+
+class FiberTower(tk.Tk):
+        def __init__(self):
+            super().__init__()
+            self.title("Fiber Tower")
+            self.geometry("500x400")
+
+            
+            ## PREFORM STEPPER SECTION 
+
+            # Frame
+            self.preform_frame = tk.LabelFrame(self, text="Preform Motor", height=100,width=150)
+            self.preform_frame.grid(row=0, column=0, rowspan=3, columnspan=3, padx=5, pady=5)
+
+            # Buttons of preform section
+
+            self.label_up = tk.Label(self.preform_frame, text="UP", font=("Segoe Ui", 12))
+            self.label_up.grid(row=0, column=0, padx=5)
+            self.button_up = ttk.Button(self.preform_frame, text=u"\u2191", command=preform_up)
+            self.button_up.grid(row=0, column=1, padx=5)
+
+            self.button_stop = tk.Button(self.preform_frame, text="Stop", command=stop_preform, font=(12), bg="red", fg="white")
+            self.button_stop.grid(row=1, column=1, padx=5)
+
+            self.label_down = tk.Label(self.preform_frame, text="DOWN", font=("Segoe Ui", 12))
+            self.label_down.grid(row=2, column=0, padx=5)
+            self.button_down = ttk.Button(self.preform_frame, text=u"\u2193", command=preform_down)
+            self.button_down.grid(row=2, column=1, padx=5)
+
+            # Printing output speed of preform motor
+
+            self.speed_preform = tk.Label(self.preform_frame, text="Speed :")
+            self.speed_preform.grid(row=3, column=0, padx=5, columnspan=2)
+
+
+            ## CAPSTAN STEPPER SECTION 
+            self.capstan_frame = tk.LabelFrame(self, text="Capstan Motor", height=120,width=150)
+            self.capstan_frame.grid(row=4, column=0, rowspan=4, columnspan=3)
+
+
+            # Creation of green Start button
+            self.button_start = tk.Button(self.capstan_frame, text="Start", command=start_capstan, font=("Segoe Ui", 12), bg="green", fg="white")
+            self.button_start.grid(row=0, column=0, padx=5)
+
+            # Creation of red Stop button
+            self.button_stop = tk.Button(self.capstan_frame, text="Stop", command=stop_capstan, font=("Segoe Ui", 12), bg="red", fg="white")
+            self.button_stop.grid(row=0, column=1, padx=5)
+
+            # Printing output speed of capstan
+
+            self.speed_capstan = tk.Label(self.capstan_frame, text="Speed:")
+            self.speed_capstan.grid(row=1, column=0, padx=5, columnspan=2)
+
+            ## SPOOL STEPPER SECTION 
+            self.spool_frame = tk.LabelFrame(self, text="Spool Motor", height=120,width=150)
+            self.spool_frame.grid(row=8, column=1, rowspan=3, columnspan=3)
+
+
+            # Creation of green Start button
+            self.spool_start = tk.Button(self.spool_frame, text="Start", command=start_spool, font=("Segoe Ui", 12), bg="green", fg="white")
+            self.spool_start.grid(row=0, column=0, padx=5)
+
+            # Creation of red Stop button
+            self.spool_stop = tk.Button(self.spool_frame, text="Stop", command=stop_spool, font=("Segoe Ui", 12), bg="red", fg="white")
+            self.spool_stop.grid(row=0, column=1, padx=5)
+
+            # Creation of yellow Reverse button
+            self.spool_stop = tk.Button(self.spool_frame, text="Reverse", command=reverse_spool, font=("Segoe Ui", 12), bg="yellow", fg="black")
+            self.spool_stop.grid(row=0, column=2, padx=5)
+
+            # Printing output speed of capstan
+
+            self.speed_spool = tk.Label(self.spool_frame, text="Speed:")
+            self.speed_spool.grid(row=1, column=0, padx=5, columnspan=2)
+
+            ## Parameter frame section 
+            self.parameter_frame = tk.LabelFrame(self, text="Parameters", height=100,width=150)
+            self.parameter_frame.grid(row=2, column=4, rowspan=3, columnspan=3, padx=5, pady=5)
+
+            # printing diameter measured by laser sensor
+            self.diameter = tk.Label(self.parameter_frame, text="Diameter Measurement:")
+            self.diameter.grid(row=0, column=0,columnspan=4, padx=5)
+
+            # input for the diameter desired
+            self.diameter_desired = tk.Label(self.parameter_frame, text='Diameter desired :')
+            self.diameter_desired.grid(row=1, column=0)
+            self.diameter_entry = tk.Entry(self.parameter_frame)
+            self.diameter_entry.grid(row=1, column=1)
+            self.diameter_entry_button = tk.Button(self.parameter_frame, text = 'Send', command=send_diameter)
+            self.diameter_entry_button.grid(row=1, column = 2)
+
+
+            ## Debug section
+            #Debug screen with time delay and received serial packets
+            self.debug_frame = tk.LabelFrame(self, text="Debug", height=100,width=150)
+            self.debug_frame.grid(row=5, column=4, rowspan=3, columnspan=3, padx=5, pady=5)
+            self.serial_print = tk.Label(self.debug_frame, text="Serial")
+            self.serial_print.grid(row=1, column=3, padx=5)
+
+
+            ##Connection frame section
+            self.connection_frame = tk.LabelFrame(self, text="Connection (WIP)", height=50,width=150)
+            self.connection_frame.grid(row=0, column=4, rowspan=2, columnspan=2, padx=5, pady=5)
+
+            # Port selection menu
+
+            self.connect_button = tk.Button(self.connection_frame, text = "Reconnect", command=reconnect)
+            self.connect_button.grid(row=0, column=1 , padx=5)
+
+            self.connect_button = tk.Button(self.connection_frame, text = "Check Ports", command=check_ports)
+            self.connect_button.grid(row=1, column=1 , padx=5)
+
+            self.status_label = tk.Label(self.connection_frame, text = "Status: Disconnected", bg = 'red')
+            self.status_label.grid(row=1, column=0 , padx=5)
+
+            self.current_port = tk.StringVar()
+
+            #If COM4 is available, it picks it as default when lauching or else it picks the first one available
+            if "COM4" in serial_ports():
+                self.current_port.set("COM4") 
+            else:
+                self.current_port.set(serial_ports()[0]) 
+
+            self.connection_drop_menu = tk.OptionMenu(self.connection_frame, self.current_port, *serial_ports())
+            self.connection_drop_menu.grid(row=0, column=0,columnspan=1, padx=5)
+
+
 
 ## Button functions
 
@@ -108,10 +238,7 @@ def check_ports():
     for port in ports:
         connection_drop_menu['menu'].add_command(label=port, command=tk._setit(current_port, port))
 
-def port_write(command):
-    """Checks if the arduino is connected then sends the command trough the serial port"""
-    if status_label.cget("text") == "Connected":
-        ser.write(command)
+
 
 #Loop functions
 
@@ -311,7 +438,7 @@ def initialise(commPort):
         status_label.config(text="Connected", bg = 'green')
 
 
-#Initialise Serial communication
+"""#Initialise Serial communication
 initialise(current_port.get())
 
 #Run the main program loop
@@ -321,4 +448,8 @@ program_loop()
 # Close serial communication 
 ser.close()
 
+"""
+tower = FiberTower()
 
+while True:
+    tower.update()
